dc8fbe16b94db0d22362e8ad3d9d528d612822f0
Stanislav <91995753+Stats-M@users.noreply.github.com>
2022-03-27 22:20:31 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -2,24 +2,36 @@
 
 #include <cmath>
 
-struct Coordinates {
-    double lat;
-    double lng;
-    bool operator==(const Coordinates& other) const {
+namespace transport_catalogue::geo
+{
+
+// Структура, определяющая положение точки в пространстве
+// и методы работы с ней
+struct Coordinates
+{
+    double lat;  // Широта
+    double lng;  // Долгота
+    bool operator==(const Coordinates& other) const
+    {
         return lat == other.lat && lng == other.lng;
     }
-    bool operator!=(const Coordinates& other) const {
+    bool operator!=(const Coordinates& other) const
+    {
         return !(*this == other);
     }
 };
 
-inline double ComputeDistance(Coordinates from, Coordinates to) {
+inline double ComputeDistance(Coordinates from, Coordinates to)
+{
     using namespace std;
-    if (from == to) {
+    if (from == to)
+    {
         return 0;
     }
     static const double dr = 3.1415926535 / 180.;
     return acos(sin(from.lat * dr) * sin(to.lat * dr)
                 + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
         * 6371000;
+}
+
 }
