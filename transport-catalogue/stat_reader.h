e7f39b48f0a03fff0ad50cd094651fd48bdb3609
Stanislav <91995753+Stats-M@users.noreply.github.com>
2022-03-27 22:25:19 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,19 @@
-// место для вашего кода
+#pragma once
+#include "transport_catalogue.h"    // структуры данных
+
+#include <utility>          // for std::pair<>
+#include <string>
+#include <string_view>      // for string manipulation via sv
+#include <istream>          // for istream
+#include <iostream>         // for cout
+
+// напишите решение с нуля
+// код сохраните в свой git-репозиторий
+
+namespace transport_catalogue::stat_reader
+{
+
+// Производит чтение запросов к справочнику
+void ProcessRequest(TransportCatalogue&, std::istream&);
+
+}
