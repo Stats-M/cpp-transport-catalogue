61fd4025e8bc70c5055e5902973ce5737565b02f
Stanislav <91995753+Stats-M@users.noreply.github.com>
2022-04-24 15:11:41 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,43 +1,43 @@
 #pragma once
-#include "geo.h"           // для работы с координатами остановок
-#include "domain.h"        // классы основных сущностей, описывают автобусы и остановки
+#include "geo.h"           // РґР»СЏ СЂР°Р±РѕС‚С‹ СЃ РєРѕРѕСЂРґРёРЅР°С‚Р°РјРё РѕСЃС‚Р°РЅРѕРІРѕРє
+#include "domain.h"        // РєР»Р°СЃСЃС‹ РѕСЃРЅРѕРІРЅС‹С… СЃСѓС‰РЅРѕСЃС‚РµР№, РѕРїРёСЃС‹РІР°СЋС‚ Р°РІС‚РѕР±СѓСЃС‹ Рё РѕСЃС‚Р°РЅРѕРІРєРё
 
 #include <deque>
-#include <map>             // для словаря координат рендерера карт
+#include <map>             // РґР»СЏ СЃР»РѕРІР°СЂСЏ РєРѕРѕСЂРґРёРЅР°С‚ СЂРµРЅРґРµСЂРµСЂР° РєР°СЂС‚
 #include <vector>
 #include <string>
 #include <string_view>
 #include <set>
-#include <ostream>         // для перегрузки оператора вывода
-#include <sstream>         // для stringstream
-#include <iomanip>         // для управления выводом (манипуляторы)
+#include <ostream>         // РґР»СЏ РїРµСЂРµРіСЂСѓР·РєРё РѕРїРµСЂР°С‚РѕСЂР° РІС‹РІРѕРґР°
+#include <sstream>         // РґР»СЏ stringstream
+#include <iomanip>         // РґР»СЏ СѓРїСЂР°РІР»РµРЅРёСЏ РІС‹РІРѕРґРѕРј (РјР°РЅРёРїСѓР»СЏС‚РѕСЂС‹)
 #include <unordered_set>
 #include <unordered_map>
-#include <algorithm>       // для uniq(), find()
+#include <algorithm>       // РґР»СЏ uniq(), find()
 #include <utility>         // for std::pair<>
 #include <cctype>          // for isspace() function
 
 namespace transport_catalogue
 {
 
-// ----------- Секция типов для ответов на запросы---------------
+// ----------- РЎРµРєС†РёСЏ С‚РёРїРѕРІ РґР»СЏ РѕС‚РІРµС‚РѕРІ РЅР° Р·Р°РїСЂРѕСЃС‹---------------
 
-// Структура ответа на запросы типа Информация об остановке и ее маршрутах
+// РЎС‚СЂСѓРєС‚СѓСЂР° РѕС‚РІРµС‚Р° РЅР° Р·Р°РїСЂРѕСЃС‹ С‚РёРїР° РРЅС„РѕСЂРјР°С†РёСЏ РѕР± РѕСЃС‚Р°РЅРѕРІРєРµ Рё РµРµ РјР°СЂС€СЂСѓС‚Р°С…
 struct StopStat
 {
-	// Параметризованный конструктор
+	// РџР°СЂР°РјРµС‚СЂРёР·РѕРІР°РЅРЅС‹Р№ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ
 	explicit StopStat(std::string_view, std::set<std::string_view>&);
 	std::string_view name;
-	std::set<std::string_view> buses;  // Должны быть отсортированными
+	std::set<std::string_view> buses;  // Р”РѕР»Р¶РЅС‹ Р±С‹С‚СЊ РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹РјРё
 };
 
-// Тип: константный указатель на статистику об остановке и ее маршрутах
+// РўРёРї: РєРѕРЅСЃС‚Р°РЅС‚РЅС‹Р№ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° СЃС‚Р°С‚РёСЃС‚РёРєСѓ РѕР± РѕСЃС‚Р°РЅРѕРІРєРµ Рё РµРµ РјР°СЂС€СЂСѓС‚Р°С…
 using StopStatPtr = const StopStat*;
 
-// Структура ответа на запросы типа Информация о маршруте
+// РЎС‚СЂСѓРєС‚СѓСЂР° РѕС‚РІРµС‚Р° РЅР° Р·Р°РїСЂРѕСЃС‹ С‚РёРїР° РРЅС„РѕСЂРјР°С†РёСЏ Рѕ РјР°СЂС€СЂСѓС‚Рµ
 struct RouteStat
 {	
-	// Параметризованный конструктор
+	// РџР°СЂР°РјРµС‚СЂРёР·РѕРІР°РЅРЅС‹Р№ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ
 	explicit RouteStat(size_t, size_t, int64_t, double, std::string_view);
 	size_t stops_on_route = 0;
 	size_t unique_stops = 0;
@@ -46,7 +46,7 @@ struct RouteStat
 	std::string name;
 };
 
-// Тип: константный указатель на статистику о маршруте
+// РўРёРї: РєРѕРЅСЃС‚Р°РЅС‚РЅС‹Р№ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° СЃС‚Р°С‚РёСЃС‚РёРєСѓ Рѕ РјР°СЂС€СЂСѓС‚Рµ
 using RouteStatPtr = const RouteStat*;
 
 // ----------- TransportCatalogue ---------------
@@ -57,35 +57,35 @@ public:
 	TransportCatalogue();
 	~TransportCatalogue();
 
-	void AddStop(Stop&&);              // Добавляет остановку в словарь всех остановок
-	void AddRoute(Route&&);            // Добавляет маршрут в словарь всех маршрутов
-	void AddDistance(StopPtr, StopPtr, size_t);    // Добавляет расстояние между двумя остановками в словарь
-	size_t GetDistance(StopPtr, StopPtr);          // Возвращает расстояние (size_t метры) между двумя остановками с перестановкой пары
-	size_t GetDistanceDirectly(StopPtr, StopPtr);  // Возвращает расстояние (size_t метры) между двумя остановками без перестановки пары
+	void AddStop(Stop&&);              // Р”РѕР±Р°РІР»СЏРµС‚ РѕСЃС‚Р°РЅРѕРІРєСѓ РІ СЃР»РѕРІР°СЂСЊ РІСЃРµС… РѕСЃС‚Р°РЅРѕРІРѕРє
+	void AddRoute(Route&&);            // Р”РѕР±Р°РІР»СЏРµС‚ РјР°СЂС€СЂСѓС‚ РІ СЃР»РѕРІР°СЂСЊ РІСЃРµС… РјР°СЂС€СЂСѓС‚РѕРІ
+	void AddDistance(StopPtr, StopPtr, size_t);    // Р”РѕР±Р°РІР»СЏРµС‚ СЂР°СЃСЃС‚РѕСЏРЅРёРµ РјРµР¶РґСѓ РґРІСѓРјСЏ РѕСЃС‚Р°РЅРѕРІРєР°РјРё РІ СЃР»РѕРІР°СЂСЊ
+	size_t GetDistance(StopPtr, StopPtr);          // Р’РѕР·РІСЂР°С‰Р°РµС‚ СЂР°СЃСЃС‚РѕСЏРЅРёРµ (size_t РјРµС‚СЂС‹) РјРµР¶РґСѓ РґРІСѓРјСЏ РѕСЃС‚Р°РЅРѕРІРєР°РјРё СЃ РїРµСЂРµСЃС‚Р°РЅРѕРІРєРѕР№ РїР°СЂС‹
+	size_t GetDistanceDirectly(StopPtr, StopPtr);  // Р’РѕР·РІСЂР°С‰Р°РµС‚ СЂР°СЃСЃС‚РѕСЏРЅРёРµ (size_t РјРµС‚СЂС‹) РјРµР¶РґСѓ РґРІСѓРјСЏ РѕСЃС‚Р°РЅРѕРІРєР°РјРё Р±РµР· РїРµСЂРµСЃС‚Р°РЅРѕРІРєРё РїР°СЂС‹
 
-	StopPtr GetStopByName(const std::string_view) const;    // Возвращает указатель на остановку по ее имени из словаря остановок
-	RoutePtr GetRouteByName(const std::string_view) const;  // Возвращает указатель на маршрут по его имени из словаря маршрутов
+	StopPtr GetStopByName(const std::string_view) const;    // Р’РѕР·РІСЂР°С‰Р°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РѕСЃС‚Р°РЅРѕРІРєСѓ РїРѕ РµРµ РёРјРµРЅРё РёР· СЃР»РѕРІР°СЂСЏ РѕСЃС‚Р°РЅРѕРІРѕРє
+	RoutePtr GetRouteByName(const std::string_view) const;  // Р’РѕР·РІСЂР°С‰Р°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РјР°СЂС€СЂСѓС‚ РїРѕ РµРіРѕ РёРјРµРЅРё РёР· СЃР»РѕРІР°СЂСЏ РјР°СЂС€СЂСѓС‚РѕРІ
 
-	RouteStatPtr GetRouteInfo(const std::string_view) const;        // Возвращает указатель на результат запроса о маршруте
-	StopStatPtr GetBusesForStopInfo(const std::string_view) const;  // Возвращает указатель на результат запроса об автобусах для останоки
+	RouteStatPtr GetRouteInfo(const std::string_view) const;        // Р’РѕР·РІСЂР°С‰Р°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° СЂРµР·СѓР»СЊС‚Р°С‚ Р·Р°РїСЂРѕСЃР° Рѕ РјР°СЂС€СЂСѓС‚Рµ
+	StopStatPtr GetBusesForStopInfo(const std::string_view) const;  // Р’РѕР·РІСЂР°С‰Р°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° СЂРµР·СѓР»СЊС‚Р°С‚ Р·Р°РїСЂРѕСЃР° РѕР± Р°РІС‚РѕР±СѓСЃР°С… РґР»СЏ РѕСЃС‚Р°РЅРѕРєРё
 
-	void GetAllRoutes(std::map<const std::string, RendererData>&) const;    // Возвращает словарь маршрутов с их остановками
+	void GetAllRoutes(std::map<const std::string, RendererData>&) const;    // Р’РѕР·РІСЂР°С‰Р°РµС‚ СЃР»РѕРІР°СЂСЊ РјР°СЂС€СЂСѓС‚РѕРІ СЃ РёС… РѕСЃС‚Р°РЅРѕРІРєР°РјРё
 
 private:
-	std::deque<Stop> all_stops_data_;                                     // Дек с информацией обо всех остановках (реальные данные, не указатели)
-	std::unordered_map<std::string_view, StopPtr> all_stops_map_;         // Словарь остановок (словарь с хэш-функцией)
-	std::deque<Route> all_buses_data_;                                    // Дек с информацией обо всех маршрутах
-	std::unordered_map<std::string_view, RoutePtr> all_buses_map_;        // Словарь маршрутов (автобусов) (словарь с хэш-функцией)
-	std::unordered_map<std::pair<StopPtr, StopPtr>, size_t, PairPointersHasher> distances_map_;    // Словарь расстояний между остановками
+	std::deque<Stop> all_stops_data_;                                     // Р”РµРє СЃ РёРЅС„РѕСЂРјР°С†РёРµР№ РѕР±Рѕ РІСЃРµС… РѕСЃС‚Р°РЅРѕРІРєР°С… (СЂРµР°Р»СЊРЅС‹Рµ РґР°РЅРЅС‹Рµ, РЅРµ СѓРєР°Р·Р°С‚РµР»Рё)
+	std::unordered_map<std::string_view, StopPtr> all_stops_map_;         // РЎР»РѕРІР°СЂСЊ РѕСЃС‚Р°РЅРѕРІРѕРє (СЃР»РѕРІР°СЂСЊ СЃ С…СЌС€-С„СѓРЅРєС†РёРµР№)
+	std::deque<Route> all_buses_data_;                                    // Р”РµРє СЃ РёРЅС„РѕСЂРјР°С†РёРµР№ РѕР±Рѕ РІСЃРµС… РјР°СЂС€СЂСѓС‚Р°С…
+	std::unordered_map<std::string_view, RoutePtr> all_buses_map_;        // РЎР»РѕРІР°СЂСЊ РјР°СЂС€СЂСѓС‚РѕРІ (Р°РІС‚РѕР±СѓСЃРѕРІ) (СЃР»РѕРІР°СЂСЊ СЃ С…СЌС€-С„СѓРЅРєС†РёРµР№)
+	std::unordered_map<std::pair<StopPtr, StopPtr>, size_t, PairPointersHasher> distances_map_;    // РЎР»РѕРІР°СЂСЊ СЂР°СЃСЃС‚РѕСЏРЅРёР№ РјРµР¶РґСѓ РѕСЃС‚Р°РЅРѕРІРєР°РјРё
 
-	// Возвращает string_view с именем остановки по указателю на экземпляр структуры Stop
+	// Р’РѕР·РІСЂР°С‰Р°РµС‚ string_view СЃ РёРјРµРЅРµРј РѕСЃС‚Р°РЅРѕРІРєРё РїРѕ СѓРєР°Р·Р°С‚РµР»СЋ РЅР° СЌРєР·РµРјРїР»СЏСЂ СЃС‚СЂСѓРєС‚СѓСЂС‹ Stop
 	std::string_view GetStopName(StopPtr stop_ptr);
-	// Возвращает string_view с именем остановки для экземпляра структуры Stop
+	// Р’РѕР·РІСЂР°С‰Р°РµС‚ string_view СЃ РёРјРµРЅРµРј РѕСЃС‚Р°РЅРѕРІРєРё РґР»СЏ СЌРєР·РµРјРїР»СЏСЂР° СЃС‚СЂСѓРєС‚СѓСЂС‹ Stop
 	std::string_view GetStopName(const Stop stop);
 
-	// Возвращает string_view с номером автобуса по указателю на экземпляр структуры Route
+	// Р’РѕР·РІСЂР°С‰Р°РµС‚ string_view СЃ РЅРѕРјРµСЂРѕРј Р°РІС‚РѕР±СѓСЃР° РїРѕ СѓРєР°Р·Р°С‚РµР»СЋ РЅР° СЌРєР·РµРјРїР»СЏСЂ СЃС‚СЂСѓРєС‚СѓСЂС‹ Route
 	std::string_view GetBusName(RoutePtr route_ptr);
-	// Возвращает string_view с номером автобуса для экземпляра структуры Route
+	// Р’РѕР·РІСЂР°С‰Р°РµС‚ string_view СЃ РЅРѕРјРµСЂРѕРј Р°РІС‚РѕР±СѓСЃР° РґР»СЏ СЌРєР·РµРјРїР»СЏСЂР° СЃС‚СЂСѓРєС‚СѓСЂС‹ Route
 	std::string_view GetBusName(const Route route);
 };
 }
