b3f13b065d72801b44351c7d0ce686db3d777cb4
Stanislav <91995753+Stats-M@users.noreply.github.com>
2022-05-14 01:14:55 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,11 +1,15 @@
 #include "request_handler.h"    // "фасад" транспортного справочника
 #include "json_reader.h"        // работа с данными в формате json
+#include "json_builder.h"
 #include "map_renderer.h"
 
 #include <iostream>             // для std::cin (isteam) и std::cout (osteam)
 
+// Для отладки установить настройку в IDE
 // Свойства конфигурации / Отладка / Аргументы команды:
 // "<input.json 1>stdout.txt"
+
+using namespace std;
 
 int main()
 {
@@ -19,7 +23,58 @@ int main()
      * Вывести в stdout ответы в виде JSON
      */
 
-    // Создаем справочник
+
+
+
+    // СЕКЦИЯ ТЕСТОВ
+
+    // Не должно компилироваться
+    /*
+    json::Builder{}.StartDict().Build();  // правило 3
+    json::Builder{}.StartDict().Key("1"s).Value(1).Value(1);  // правило 2
+    json::Builder{}.StartDict().Key("1"s).Key(""s);  // правило 1
+    json::Builder{}.StartArray().Key("1"s);  // правило 4
+    json::Builder{}.StartArray().EndDict();  // правило 4
+    json::Builder{}.StartArray().Value(1).Value(2).EndDict();  // правило 5
+    
+    // Должно компилироваться
+    json::Print(
+        json::Document{
+            json::Builder{}
+            .StartDict()
+                .Key("key1"s).Value(123)
+                .Key("key2"s).Value("value2"s)
+                .Key("key3"s).StartArray()
+                    .Value(456)
+                    .StartDict().EndDict()
+                    .StartDict()
+                        .Key(""s)
+                        .Value(nullptr)
+                    .EndDict()
+                    .Value(""s)
+                .EndArray()
+            .EndDict()
+            .Build()
+        },
+        std::cout
+    );
+    std::cout << endl;
+
+    json::Print(
+        json::Document{
+            json::Builder{}
+            .Value("just a string"s)
+            .Build()
+        },
+        std::cout
+    );
+    std::cout << endl;
+    */
+
+    // КОНЕЦ СЕКЦИИ ТЕСТОВ
+
+
+     // Создаем справочник
     transport_catalogue::TransportCatalogue tc;
     // Создаем рендерер карт
     map_renderer::MapRenderer mr;
