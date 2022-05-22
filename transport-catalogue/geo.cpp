d75ac83570cbb0675426bfbd7658812071e4905b
Stanislav <91995753+Stats-M@users.noreply.github.com>
2022-05-22 20:08:38 +0300

    Update geo.cpp

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -7,12 +7,12 @@
 namespace geo
 {
 
-bool geo::Coordinates::operator==(const Coordinates& other) const
+bool Coordinates::operator==(const Coordinates& other) const
 {
     return lat == other.lat && lng == other.lng;
 }
 
-bool geo::Coordinates::operator!=(const Coordinates& other) const
+bool Coordinates::operator!=(const Coordinates& other) const
 {
     return !(*this == other);
 }
