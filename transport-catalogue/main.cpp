1dcd5c796836749cda85a5e4b5e595db85078067
Stanislav <91995753+Stats-M@users.noreply.github.com>
2022-05-22 20:16:23 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -21,60 +21,9 @@ int main()
      * Выполнить запросы к справочнику, находящиеся в массиве "stat_requests", построив JSON-массив
      * с ответами.
      * Вывести в stdout ответы в виде JSON
-     */
-
-
-
-
-    // СЕКЦИЯ ТЕСТОВ
-
-    // Не должно компилироваться
-    /*
-    json::Builder{}.StartDict().Build();  // правило 3
-    json::Builder{}.StartDict().Key("1"s).Value(1).Value(1);  // правило 2
-    json::Builder{}.StartDict().Key("1"s).Key(""s);  // правило 1
-    json::Builder{}.StartArray().Key("1"s);  // правило 4
-    json::Builder{}.StartArray().EndDict();  // правило 4
-    json::Builder{}.StartArray().Value(1).Value(2).EndDict();  // правило 5
-    
-    // Должно компилироваться
-    json::Print(
-        json::Document{
-            json::Builder{}
-            .StartDict()
-                .Key("key1"s).Value(123)
-                .Key("key2"s).Value("value2"s)
-                .Key("key3"s).StartArray()
-                    .Value(456)
-                    .StartDict().EndDict()
-                    .StartDict()
-                        .Key(""s)
-                        .Value(nullptr)
-                    .EndDict()
-                    .Value(""s)
-                .EndArray()
-            .EndDict()
-            .Build()
-        },
-        std::cout
-    );
-    std::cout << endl;
-
-    json::Print(
-        json::Document{
-            json::Builder{}
-            .Value("just a string"s)
-            .Build()
-        },
-        std::cout
-    );
-    std::cout << endl;
     */
 
-    // КОНЕЦ СЕКЦИИ ТЕСТОВ
-
-
-     // Создаем справочник
+    // Создаем справочник
     transport_catalogue::TransportCatalogue tc;
     // Создаем рендерер карт
     map_renderer::MapRenderer mr;
